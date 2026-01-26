2553b8123c4c73941c0ce1c8c30b90faa33eae86
John Blum <jblum0@gmail.com>
2026-01-25 21:58:46 -0800

    Implement ChromaNormalizer

--- chromaprint-java/src/main/java/org/acoustid/chromaprint/ChromaNormalizer.java
+++ chromaprint-java/src/main/java/org/acoustid/chromaprint/ChromaNormalizer.java
@@ -2,10 +2,18 @@
 // Distributed under the MIT license, see the LICENSE file for details.
 package org.acoustid.chromaprint;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
+/**
+ * @author Lukas Lalinsky
+ * @author OpenAI (ChatGPT 5.2)
+ * @author John Blum
+ * @see FeatureVectorConsumer
+ */
 public class ChromaNormalizer implements FeatureVectorConsumer {
+
+    private static final double DEFAULT_THRESHOLD = 0.01d;
 
     private final FeatureVectorConsumer consumer;
 
@@ -15,10 +23,32 @@ public class ChromaNormalizer implements FeatureVectorConsumer {
 
     @Override
     public void consume(List<Double> features) {
-        // Normalize features
-        List<Double> normalized = new ArrayList<>(features);
-        // TODO: Simplified normalization - full implementation would normalize vector
-        this.consumer.consume(normalized);
+        double euclideanNorm = euclideanNorm(features);
+        List<Double> normalizedFeatures = normalizeValues(features, euclideanNorm, DEFAULT_THRESHOLD);
+        this.consumer.consume(normalizedFeatures);
+    }
+
+    private double euclideanNorm(List<Double> values) {
+
+        double squaresSum = values.stream()
+            .map(this::squared)
+            .reduce(Double::sum)
+            .orElse(0.0d);
+
+        return squaresSum > 0.0d
+            ? Math.sqrt(squaresSum)
+            : 0.0d;
+    }
+
+    private List<Double> normalizeValues(List<Double> values, double norm, double threshold) {
+
+        return norm >= threshold
+            ? values.stream().map(value -> value / norm).toList()
+            : Collections.nCopies(values.size(), 0.0d);
+    }
+
+    private double squared(double value) {
+        return value * value;
     }
 
     public void reset() {
