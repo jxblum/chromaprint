cd12fe4ab6f95d9916176e4ad2d9b55ea0fec7a1
John Blum <jblum0@gmail.com>
2026-01-25 22:37:41 -0800

    Convert FFTFrame to Java record

--- chromaprint-java/src/main/java/org/acoustid/chromaprint/FFTFrame.java
+++ chromaprint-java/src/main/java/org/acoustid/chromaprint/FFTFrame.java
@@ -1,16 +1,15 @@
 // Copyright (C) 2010-2016  Lukas Lalinsky
 // Distributed under the MIT license, see the LICENSE file for details.
-
 package org.acoustid.chromaprint;
 
-public class FFTFrame {
-    private double[] data;
-    
-    public FFTFrame(double[] data) {
-        this.data = data;
-    }
-    
-    public double[] getData() {
-        return data;
-    }
+/**
+ * Fast Fourier Transformation Frame
+ *
+ * @author Lukas Lalinsky
+ * @author Cursor AI
+ * @author John Blum
+ * @param data double array of data in this frame.
+ */
+public record FFTFrame(double[] data) {
+
 }
